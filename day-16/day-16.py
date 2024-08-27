e0cb5818db55cc59fcf8ce2e7dd6b776fabdcc2a
aivanets <aivanets@ukaachen.de>
2024-08-27 15:46:12 +0200

    Submit Part 2

--- day-16/day-16.py
+++ day-16/day-16.py
@@ -16,11 +16,14 @@ class Beam:
 
 
 class Tile:
-    def __init__(self, x, y, value, symbol):
+    def __init__(self, x, y, symbol):
         self.x = x
         self.y = y
-        self.value = value
+        self.directions = set()
         self.symbol = symbol
+
+    def add_direction(self, direction):
+        self.directions.add(direction)
 
 
 def read_input(filename):
@@ -33,7 +36,7 @@ def prepare_input(input_list):
     for y, line in enumerate(input_list):
         tiles_line = []
         for x, symbol in enumerate(line):
-            tiles_line.append(Tile(x, y, 0, symbol))
+            tiles_line.append(Tile(x, y, symbol))
         tiles.append(tiles_line)
     return tiles
 
@@ -41,18 +44,21 @@ def prepare_input(input_list):
 def show_tiles(tiles):
     for line in tiles:
         for tile in line:
-            print(tile.symbol if tile.value == 0 else "#", end="")
+            print(tile.symbol if len(tile.directions) == 0 else "#", end="")
         print()
     print()
 
-def process_beam(beam, tiles, beams):
+def process_beam(beam, tiles):
     if beam.x < 0 or beam.y < 0:
         return None
     if beam.y >= len(tiles) or beam.x >= len(tiles[0]):
         return None
     current_tile = tiles[beam.y][beam.x]
     # Energize the tile
-    current_tile.value += 1
+    if beam.direction in current_tile.directions:
+        return None
+    else:
+        current_tile.add_direction(beam.direction)
 
     if current_tile.symbol == ".":
         beam.move()
@@ -94,26 +100,55 @@ def process_beam(beam, tiles, beams):
 
     return None
 
-
-def main():
-    input_list = read_input("day16.txt")
-    tiles = prepare_input(input_list)
-
-    first_beam = Beam(0, 0, "right")
+def start_cycle(first_beam, tiles):
     beams = [first_beam]
-    for _ in range(1000):
+    while len(beams) > 0:
         new_beams = []
         for beam in beams:
-            new_beam = process_beam(beam, tiles, beams)
+            new_beam = process_beam(beam, tiles)
             if new_beam is not None:
                 if isinstance(new_beam, list):
                     new_beams.extend(new_beam)
                 else:
                     new_beams.append(new_beam)
         beams = new_beams
+    return tiles
 
-    print(sum(map(lambda y: 1, filter(lambda x: x.value > 0, [tile for line in tiles for tile in line]))))
+def main():
+    input_list = read_input("day16.txt")
+    init_tiles = prepare_input(input_list)
+    # Up
+    upper_number = []
+    for tile in init_tiles[0]:
+        tiles = prepare_input(input_list)
+        first_beam = Beam(tile.x, tile.y, "down")
+        tiles = start_cycle(first_beam, tiles)
+        upper_number.append(sum(map(lambda y: 1, filter(lambda x: len(x.directions) > 0, [tile for line in tiles for tile in line]))))
 
-
+    print(max(upper_number))
+    # Left
+    left_number = []
+    for tile in init_tiles:
+        tiles = prepare_input(input_list)
+        first_beam = Beam(tile[0].x, tile[0].y, "right")
+        tiles = start_cycle(first_beam, tiles)
+        left_number.append(sum(map(lambda y: 1, filter(lambda x: len(x.directions) > 0, [tile for line in tiles for tile in line]))))
+    print(max(left_number))
+    # Right
+    right_number = []
+    for tile in init_tiles:
+        tiles = prepare_input(input_list)
+        first_beam = Beam(tile[-1].x, tile[-1].y, "left")
+        tiles = start_cycle(first_beam, tiles)
+        right_number.append(sum(map(lambda y: 1, filter(lambda x: len(x.directions) > 0, [tile for line in tiles for tile in line]))))
+    print(max(right_number))
+    # Down
+    down_number = []
+    for tile in init_tiles[-1]:
+        tiles = prepare_input(input_list)
+        first_beam = Beam(tile.x, tile.y, "up")
+        tiles = start_cycle(first_beam, tiles)
+        down_number.append(sum(map(lambda y: 1, filter(lambda x: len(x.directions) > 0, [tile for line in tiles for tile in line]))))
+    print(max(down_number))
 if __name__ == "__main__":
     main()
